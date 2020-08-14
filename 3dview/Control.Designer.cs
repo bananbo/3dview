8a5b3637e6accd5f12958faeb5ec8a5d5daa8a28
bananbo <bananboba@yahoo.co.jp>
2020-08-14 21:19:15 +0900

    プロジェクト ファイルを追加します。

---
+++ 3dview/Control.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _3dview
+{
+    partial class Control
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ビューアの表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "形状の表示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(247, 87);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Control
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Control";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 3dview/Viewer.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+// openTK
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+// surfaceAnalyzer
+using SurfaceAnalyzer;
+
+
+
+namespace _3dview
+{
+    public partial class Viewer : Form
+    {
+        #region Camera__Field
+
+        bool isCameraRotating;      //カメラが回転状態かどうか
+        Vector2 current, previous;  //現在の点、前の点
+        float zoom = 1.0f;                 //拡大度
+        double rotateX = 1, rotateY = 0, rotateZ = 0;//カメラの回転による移動
+        float theta = 0;
+        float phi = 0;
+
+        #endregion
+
+        public Viewer()
+        {
+            InitializeComponent();
+
+            AddglControl();
+        }
+
+        // glControlの追加
+        GLControl glControl;
+        private void AddglControl()
+        {
+            SuspendLayout();
+
+            int width = this.Width;
+            int height = this.Height;
+
+            //GLControlの初期化
+            glControl = new GLControl();
+
+            glControl.Name = "SHAPE";
+            glControl.Size = new Size(width, height);
+            glControl.Location = new System.Drawing.Point(0, 0);
+            glControl.SendToBack();
+
+            //イベントハンドラ
+            glControl.Load += new EventHandler(glControl_Load);
+            glControl.Resize += new EventHandler(glControl_Resize);
+            glControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this._3DView_MouseDown);
+            glControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this._3DView_MouseMove);
+            glControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this._3DView_MouseUp);
+            glControl.MouseWheel += new System.Windows.Forms.MouseEventHandler(this._3DView_MouseWheel);
+
+            Controls.Add(glControl);
+
+            ResumeLayout(false);
+
+        }
+
+        private void glControl_Load(object sender, EventArgs e)
+        {
+            GLControl s = (GLControl)sender;
+            s.MakeCurrent();
+
+            GL.ClearColor(Color4.White);
+            GL.Enable(EnableCap.DepthTest);
+
+            Update();
+        }
+
+        private void glControl_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, glControl.Size.Width, glControl.Size.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4,
+                (float)glControl.Size.Width / (float)glControl.Size.Height, 1.0f, 256.0f);
+            GL.LoadMatrix(ref projection);
+
+            Update();
+        }
+
+        private void _3DView_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            // 右ボタンが押された場合
+            if (e.Button == MouseButtons.Right)
+            {
+                isCameraRotating = true;
+                current = new Vector2(e.X, e.Y);
+            }
+            Update();
+        }
+
+
+        private void _3DView_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //右ボタンが押された場合
+            if (e.Button == MouseButtons.Right)
+            {
+                isCameraRotating = false;
+                previous = Vector2.Zero;
+            }
+            Update();
+        }
+
+
+        private void _3DView_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            // カメラが回転状態の場合
+            if (isCameraRotating)
+            {
+                previous = current;
+                current = new Vector2(e.X, e.Y);
+                Vector2 delta = current - previous;
+                delta /= (float)Math.Sqrt(this.Width * this.Width + this.Height * this.Height);
+                float length = delta.Length;
+
+                if (length > 0.0)
+                {
+                    theta += delta.X * 10;
+                    phi += delta.Y * 10;
+                    rotateX = Math.Cos(theta) * Math.Cos(phi);
+                    rotateY = Math.Sin(phi);
+                    rotateZ = Math.Sin(theta) * Math.Cos(phi);
+                }
+
+                Update();
+            }
+        }
+
+        private void _3DView_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            float delta = e.Delta;
+
+            zoom *= (float)Math.Pow(1.001, delta);
+
+            //拡大、縮小の制限
+            if (zoom > 4.0f)
+                zoom = 4.0f;
+            if (zoom < 0.03f)
+                zoom = 0.03f;
+
+            Update();
+        }
+
+        PolygonModel Polygon;
+        public void Update()
+        {
+            if (Polygon == null) return;
+            Render(Polygon);
+        }
+        public void Render(PolygonModel polygon)
+        {
+            Polygon = polygon;
+
+            // バッファのクリア
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // カメラ設定
+            Vector3 vec_rotate = new Vector3((float)rotateX, (float)rotateY, (float)rotateZ);
+            Vector3 center = new Vector3(N2TK(Polygon.GravityPoint()));
+            Vector3 eye = center + vec_rotate * center.LengthFast / zoom;
+            Matrix4 modelView = Matrix4.LookAt(eye, center, Vector3.UnitY);
+
+            // 表示設定
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelView);
+
+            // 3D形状の表示
+            DrawPolygons(polygon);
+
+            // バッファの入れ替え
+            glControl.SwapBuffers();
+        }
+
+        private void DrawPolygons(PolygonModel polygon)
+        {
+            if (polygon == null) return;
+
+            //描画
+            GL.Begin(PrimitiveType.Triangles);
+
+            //三角形を描画
+            for (int l = 0; l < polygon.Faces.Count; l++)
+            {
+                var normal = polygon.Faces[l].Normal();
+                GL.Color4(Math.Abs(normal.X), Math.Abs(normal.Y), Math.Abs(normal.Z), 0);
+                GL.Normal3(N2TK(normal));
+                GL.Vertex3(N2TK(polygon.Faces[l].Vertices[0].P));
+                GL.Vertex3(N2TK(polygon.Faces[l].Vertices[2].P));
+                GL.Vertex3(N2TK(polygon.Faces[l].Vertices[1].P));
+            }
+            GL.End();
+        }
+
+        // Numerics.Vector3をOpenTK.Vector3に変換します。
+        private static OpenTK.Vector3 N2TK(System.Numerics.Vector3 vec3) => new Vector3(vec3.X, vec3.Z, vec3.Y);
+
+        // 画像の保存
+        public OpenCvSharp.Mat GetMat()
+        {
+            int width = glControl.Width;
+            int height = glControl.Height;
+            
+            float[] floatArr = new float[width * height * 3];
+            OpenCvSharp.Mat ret = new OpenCvSharp.Mat(height, width, OpenCvSharp.MatType.CV_32FC3);
+
+            // dataBufferへの画像の読み込み
+            IntPtr dataBuffer = Marshal.AllocHGlobal(width * height * 12);
+            GL.ReadBuffer(ReadBufferMode.Front);
+            GL.ReadPixels(0, 0, width, height, PixelFormat.Bgr, PixelType.Float, dataBuffer);
+
+            // imgへの読み込み
+            Marshal.Copy(dataBuffer, floatArr, 0, floatArr.Length);
+
+            // opencvsharp.Matへの変換
+            Marshal.Copy(floatArr, 0, ret.Data, floatArr.Length);
+
+            // 破棄
+            Marshal.FreeHGlobal(dataBuffer);
+
+            return ret;
+        }
+    }
+}
